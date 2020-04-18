4f169625e665ce3d6bfc43a8977947f82782df33
sebaslds09 <ing.sebastianortiz@outlook.com>
2020-04-17 22:00:30 -0500

    Initial load

---
+++ PruebaTecnica_SebastianOrtiz/App_Start/ReactConfig.cs
@@ -0,0 +1,17 @@
+﻿using JavaScriptEngineSwitcher.Core;
+using JavaScriptEngineSwitcher.V8;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(PruebaTecnica_SebastianOrtiz.ReactConfig), "Configure")]
+
+
+namespace PruebaTecnica_SebastianOrtiz
+{
+    public static class ReactConfig
+    {
+        public static void Configure()
+        {
+            JsEngineSwitcher.Current.DefaultEngineName = V8JsEngine.EngineName;
+            JsEngineSwitcher.Current.EngineFactories.AddV8();
+        }
+    }
+}
---
+++ PruebaTecnica_SebastianOrtiz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PruebaTecnica_SebastianOrtiz.MvcApplication" Language="C#" %>
